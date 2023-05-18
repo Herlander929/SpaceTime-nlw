34ea266c34bd3a15e7c957a2cc5234299ee052f7
Herlander <herlander97dasilva@gmail.com>
2023-05-18 13:06:41 -0300

    Configurando ambiente web

---
+++ front-end/.history/src/app/page_20230518130341.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return <div className="h-screen bg-zinc-900">
+    oi
+    </div>
+}
---
+++ front-end/.history/src/app/page_20230518130429.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return <div className="h-screen bg-zinc-950 text-zinc-50">
+    <h1>sua cápsula do tempo</h1>
+    </div>
+}
